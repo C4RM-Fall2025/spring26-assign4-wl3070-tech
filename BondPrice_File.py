d5c61c84a9c535f2f06916dea16a58ffaa3182b5
wl3070-tech <wl3070@columbia.edu>
2026-02-20 19:33:53 -0500

    Update BondPrice_File.py

--- BondPrice_File.py
+++ BondPrice_File.py
@@ -1,6 +1,19 @@
 
 
 def getBondPrice(y, face, couponRate, m, ppy=1):
+    r = y/ppy
+    N = m * ppy
+    coupon = face* couponRate/ppy
+
+    bondPrice = 0.0
+
+    for t in range(1, N + 1):
+        bondPrice += coupon / (1 + r) ** t
+
+    bondPrice += face / (1 + r) ** N
+
+    return(bondPrice)
+    
     if ppy == 1:
         x = 2170604
     if ppy == 2:
