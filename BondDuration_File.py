1972a5f077cfc7ce9915c39e4e671f98ff831568
wl3070-tech <wl3070@columbia.edu>
2026-02-20 20:21:24 -0500

    Update BondDuration_File.py

--- BondDuration_File.py
+++ BondDuration_File.py
@@ -1,3 +1,28 @@
 
 def getBondDuration(y, face, couponRate, m, ppy = 1):
-    return(8.51)
+    r = y / ppy
+    N = m * ppy
+    C = face * couponRate / ppy
+
+    price = 0
+    weighted_sum = 0
+
+   for t in range(1, N):
+        pv = C / (1 + r) ** t
+        price = price + pv
+        weighted_sum = weighted_sum + t * pv
+
+    cf_last = C + face
+    pv_last = cf_last / (1 + r) ** N
+
+    price = price + pv_last
+    weighted_sum = weighted_sum + N * pv_last
+
+    duration_periods = weighted_sum / price
+    duration_years = duration_periods / ppy
+
+    bondDuration = duration_years
+
+    return(bondDuration)
+
+
